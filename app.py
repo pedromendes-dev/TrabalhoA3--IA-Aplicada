6e950805d16c047b89f8606f7fff4c86623c01c7
YuriLucas7 <lucasy973@gmail.com>
2026-05-14 15:24:25 -0300

    nova att para busca de todo banco

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ matrix, kmeans_model = apply_kmeans(matrix)
 def index():
     recommendations = None
     
-    movies_list = movies[["movieId", "title_pt", "poster_url"]].head(300).fillna("").to_dict(orient="records")
+    movies_list = movies[["movieId", "title", "title_pt", "poster_url", "genres"]].fillna("").to_dict(orient="records")
 
     if request.method == "POST":
         selected_titles = request.form.getlist("movies")
@@ -24,7 +24,7 @@ def index():
         if selected_titles:
             selected_ids = movies[movies["title_pt"].isin(selected_titles)]["movieId"].tolist()
             recs_df = recommend_for_new_user(selected_ids, matrix, kmeans_model, movies, top_n=12)
-            recommendations = recs_df.to_dict(orient="records")
+            recommendations = recs_df.fillna("").to_dict(orient="records")
 
     return render_template(
         "index.html",
